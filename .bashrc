528ab9e184cf3cef417ed05c0fd016a115a054aa
Julian Kehr <juliankehr01@gmail.com>
2025-12-07 16:26:23 +0100

    new style

--- .bashrc
+++ .bashrc
@@ -10,6 +10,7 @@ alias l='lsd -a'
 alias ls='lsd -a'
 alias grep='grep --color=auto'
 alias lg='lazygit'
+alias machsgut='shutdown -h 0'
 PS1='[\u@\h \W]\$ '
 
 export NIXPKGS_ALLOW_UNFREE=1
---
+++ setwall.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+WALLPAPER_DIR="$HOME/wallpapers/used"
+CURRENT_WALL=$(hyprctl hyprpaper listloaded)
+
+# Get a random wallpaper that is not the current one
+# WALLPAPER=$(find "$WALLPAPER_DIR" -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
+WALLPAPER="$HOME/wallpapers/snow.jpg"
+
+# Apply the selected wallpaper
+hyprctl hyprpaper reload ,"$WALLPAPER"
+
+# pywal wal
+# wal -n -e -q -i "$WALLPAPER"
+wpg -s "$WALLPAPER"
+
+# prep colors for hyprland usage
+sed -i -e 's/^/$/' ~/.cache/wal/colors-oomox
+cp ~/.cache/wal/colors-oomox ~/.config/hypr/colors.conf
+
+# waybar
+killall -SIGUSR2 waybar 
+
+# pywallfox
+pywalfox update
